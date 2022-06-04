3c59e5f87fa2ad559a6a87dee0ba89efc2e2c9f5
samiksadhu <sadhusamik@gmail.com>
2022-06-03 20:54:38 -0400

    corrections

--- espnet2/cepnet/espnet_model.py
+++ espnet2/cepnet/espnet_model.py
@@ -23,12 +23,7 @@ from espnet.nets.pytorch_backend.transformer.label_smoothing_loss import (
 from espnet2.asr.ctc import CTC
 from espnet2.asr.decoder.abs_decoder import AbsDecoder
 from espnet2.asr.encoder.abs_encoder import AbsEncoder
-from espnet2.asr.frontend.abs_frontend import AbsFrontend
-from espnet2.modnet.projector.abs_projector import AbsProjector
-from espnet2.asr.postencoder.abs_postencoder import AbsPostEncoder
-from espnet2.asr.preencoder.abs_preencoder import AbsPreEncoder
-from espnet2.asr.specaug.abs_specaug import AbsSpecAug
-from espnet2.layers.abs_normalize import AbsNormalize
+
 from espnet2.torch_utils.device_funcs import force_gatherable
 from espnet2.train.abs_espnet_model import AbsESPnetModel
 
