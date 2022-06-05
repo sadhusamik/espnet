e157e5d8472fb1cf1fbca4bea465fd9893f5dd63
samiksadhu <sadhusamik@gmail.com>
2022-06-05 17:46:39 -0400

    corrections

--- espnet2/cepnet/espnet_model.py
+++ espnet2/cepnet/espnet_model.py
@@ -122,27 +122,21 @@ class CepNet(AbsESPnetModel):
         fft_signal = fft_signal.unsqueeze(-1)  # Batch x nfft x 1
         fft_signal_original = fft_signal_original.unsqueeze(-1)  # Batch x nfft x 1
 
-        print(fft_signal.shape)
+
 
         # 1. Encoder for real and imaginary parts
         ll = torch.Tensor([int(self.nfft / 2) + 1] * batch_size)
         encoder_out_real, _, _ = self.encoder_real(torch.real(fft_signal[:, :int(self.nfft / 2) + 1, :]), ll)
-        print(encoder_out_real.shape)
+
         encoder_out_real = self.projector_real(encoder_out_real)
-        print(encoder_out_real.shape)
         encoder_out_imag, _, _ = self.encoder_imag(torch.imag(fft_signal[:, :int(self.nfft / 2) + 1, :]), ll)
         encoder_out_imag = self.projector_imag(encoder_out_imag)
 
-        print(encoder_out_real.shape)
         encoder_out_real = torch.cat((encoder_out_real, torch.flip(encoder_out_real[:, :-2], [1])), dim=1)
         encoder_out_imag = torch.cat((encoder_out_imag, -torch.flip(encoder_out_imag[:, :-2], [1])), dim=1)
 
-        print(encoder_out_real.shape)
-
         encoder_out = torch.view_as_complex(
             torch.cat((encoder_out_real.unsqueeze(-1), encoder_out_imag.unsqueeze(-1)), dim=-1))
-
-        print(encoder_out_real.shape)
 
         loss1 = self.prediction_loss(torch.real(fft_signal_original), torch.real(fft_signal - encoder_out))
         loss2 = self.prediction_loss(torch.imag(fft_signal_original), torch.imag(fft_signal - encoder_out))
