8b02ad6340a63570fd7283f1be3abd4bdc03628b
samiksadhu <sadhusamik@gmail.com>
2022-06-25 17:29:55 -0400

    corrections

--- espnet2/train/preprocessor.py
+++ espnet2/train/preprocessor.py
@@ -418,7 +418,7 @@ class CommonPreprocessorPairedSpeech(AbsPreprocessor):
         print(np.max( data['speech_original']))
         sys.stdout.flush()
         if self.speech_name in data:
-            if self.train and self.rirs is not None and self.noises is not None:
+            if self.train and self.rirs is not None or self.noises is not None:
                 speech = data[self.speech_name]
                 nsamples = len(speech)
 
