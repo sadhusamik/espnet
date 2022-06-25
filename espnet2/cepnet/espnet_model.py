da5fbf5f703c5f9a58014cd355a1b51514df2b07
samiksadhu <sadhusamik@gmail.com>
2022-06-25 18:07:49 -0400

    corrections

--- espnet2/cepnet/espnet_model.py
+++ espnet2/cepnet/espnet_model.py
@@ -208,7 +208,12 @@ class CepNet(AbsESPnetModel):
             speech_lengths: (Batch, )
 
         """
+        print('speech shape')
         print(speech.shape)
+        sys.stdout.flush()
+
+        print('speech original shape')
+        print(speech_original.shape)
         sys.stdout.flush()
         batch_size = speech.shape[0]
         sig_len = speech.shape[1]
