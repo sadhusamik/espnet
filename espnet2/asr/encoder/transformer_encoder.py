ad6e11a63e078215e2a59f3f9ecb9a9f321dfd1e
samiksadhu <sadhusamik@gmail.com>
2022-11-17 16:50:05 -0500

    log modulation spectrum

--- espnet2/asr/encoder/transformer_encoder.py
+++ espnet2/asr/encoder/transformer_encoder.py
@@ -194,6 +194,7 @@ class TransformerEncoder(AbsEncoder):
             or isinstance(self.embed, Conv2dSubsampling6)
             or isinstance(self.embed, Conv2dSubsampling8)
             or isinstance(self.embed, Conv2dMultichannel)
+            or isinstance(self.embed, Conv2dMultichannel2Channel)
         ):
             short_status, limit_size = check_short_utt(self.embed, xs_pad.size(1))
             if short_status:
