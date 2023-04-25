9816cb3fd5c439934648b59900433d3e33bfc78d
samiksadhu <sadhusamik@gmail.com>
2023-04-25 15:18:53 -0400

    some updates

--- espnet2/tasks/abs_task.py
+++ espnet2/tasks/abs_task.py
@@ -1177,10 +1177,9 @@ class AbsTask(ABC):
                         logging.info(f"Setting {k}.requires_grad = False")
                         p.requires_grad = False
 
-            vv=['frontend.upstream.upstream.model.encoder.layers.23',
+            vv = ['frontend.upstream.upstream.model.encoder.layers.23',
                 'frontend.upstream.upstream.model.encoder.layers.22',
-                'frontend.upstream.upstream.model.encoder.layers.21',
-                'frontend.upstream.upstream.model.encoder.layers.20']
+                'frontend.upstream.upstream.model.encoder.layers.21']
             for t in vv:
                 for k, p in model.named_parameters():
                     #print
